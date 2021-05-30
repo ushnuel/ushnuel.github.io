8ec71d060cc7c5d73364a5d4f0d426f8f64ce6c3
ushnuel <ezeemmanuel2010@gmail.com>
2021-05-30 14:12:38 +0100

    feature(image):add more images

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const toggleBurger = () => {
-  let burger = document.querySelector(".burger")
-  burger.classList.toggle("open")
-  let nav = document.querySelector(".nav")
-  nav.classList.toggle("open")
-  let mobileNav = document.querySelector(".mobile-nav-bg")
-  mobileNav.classList.toggle("open")
-  let navItems = document.querySelectorAll(".click")
+  let burger = document.querySelector(".burger");
+  burger.classList.toggle("open");
+  let nav = document.querySelector(".nav");
+  nav.classList.toggle("open");
+  let mobileNav = document.querySelector(".mobile-nav-bg");
+  mobileNav.classList.toggle("open");
+  let navItems = document.querySelectorAll(".click");
   Array.from(navItems).forEach((item) => {
     item.addEventListener("click", () => {
-      nav.classList.remove("open")
-      mobileNav.classList.remove("open")
-      burger.classList.remove("open")
-    })
-  })
-}
+      nav.classList.remove("open");
+      mobileNav.classList.remove("open");
+      burger.classList.remove("open");
+    });
+  });
+};
 
 $(document).ready(function () {
   // Add smooth scrolling to all links
@@ -21,10 +21,10 @@ $(document).ready(function () {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault()
+      event.preventDefault();
 
       // Store hash
-      var hash = this.hash
+      var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
@@ -35,15 +35,15 @@ $(document).ready(function () {
         800,
         function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash
+          window.location.hash = hash;
         }
-      )
+      );
     } // End if
-  })
-})
+  });
+});
 
-gsap.registerPlugin(ScrollTrigger)
-let tl = gsap.timeline()
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline();
 
 tl.from(".name-stg", {
   y: "-300%",
@@ -51,19 +51,8 @@ tl.from(".name-stg", {
   duration: 2,
   ease: Power4.easeOut,
   stagger: 0.9,
-})
+});
 
-tl.from(
-  ".stagger1",
-  {
-    opacity: 0,
-    y: -50,
-    stagger: 0.3,
-    ease: Power4.easeOut,
-    duration: 2,
-  },
-  "-=1.2"
-)
 tl.from(
   ".stagger2",
   {
@@ -74,7 +63,7 @@ tl.from(
     duration: 2,
   },
   "-=2"
-)
+);
 
 gsap.from(".transition2", {
   scrollTrigger: {
@@ -85,7 +74,8 @@ gsap.from(".transition2", {
   opacity: 0,
   duration: 1.2,
   stagger: 0.3,
-})
+});
+
 gsap.from(".transition3", {
   scrollTrigger: {
     trigger: ".transition3",
@@ -95,7 +85,7 @@ gsap.from(".transition3", {
   opacity: 0,
   duration: 1.2,
   stagger: 0.3,
-})
+});
 gsap.from(".transition4", {
   scrollTrigger: {
     trigger: ".transition4",
@@ -105,7 +95,7 @@ gsap.from(".transition4", {
   opacity: 0,
   duration: 1.2,
   stagger: 0.3,
-})
+});
 
 gsap.from(".transition5", {
   scrollTrigger: {
@@ -116,7 +106,7 @@ gsap.from(".transition5", {
   opacity: 0,
   duration: 1.2,
   stagger: 0.3,
-})
+});
 
 gsap.from(".transition7", {
   scrollTrigger: {
@@ -128,7 +118,7 @@ gsap.from(".transition7", {
   duration: 1,
   stagger: 0.1,
   ease: Sine.easeOut,
-})
+});
 
 gsap.from(
   ".transition9",
@@ -143,7 +133,7 @@ gsap.from(
     ease: Sine.easeOut,
   },
   "+2"
-)
+);
 
 gsap.from(".left", {
   scrollTrigger: {
@@ -154,7 +144,7 @@ gsap.from(".left", {
   opacity: 0,
   duration: 1,
   ease: Sine.easeOut,
-})
+});
 
 tl.from(".transition8", {
   opacity: 0,
@@ -162,18 +152,20 @@ tl.from(".transition8", {
   stagger: 0.3,
   ease: Power4.easeOut,
   duration: 2,
-})
+});
 
 gsap.from(".showProject1", {
   scrollTrigger: {
     trigger: ".showProject1",
+    toggleActions: "restart none none none",
     start: "top center",
   },
   x: 80,
   opacity: 0,
   duration: 3,
   ease: Power4.easeOut,
-})
+});
+
 gsap.from(".showProject2", {
   scrollTrigger: {
     trigger: ".showProject2",
@@ -183,7 +175,8 @@ gsap.from(".showProject2", {
   opacity: 0,
   duration: 3,
   ease: Power4.easeOut,
-})
+});
+
 gsap.from(".showProject3", {
   scrollTrigger: {
     trigger: ".showProject3",
@@ -193,7 +186,8 @@ gsap.from(".showProject3", {
   opacity: 0,
   duration: 3,
   ease: Power4.easeOut,
-})
+});
+
 gsap.from(".showProject4", {
   scrollTrigger: {
     trigger: ".showProject4",
@@ -203,7 +197,7 @@ gsap.from(".showProject4", {
   opacity: 0,
   duration: 3,
   ease: Power4.easeOut,
-})
+});
 gsap.from(".showProject5", {
   scrollTrigger: {
     trigger: ".showProject5",
@@ -213,4 +207,4 @@ gsap.from(".showProject5", {
   opacity: 0,
   duration: 3,
   ease: Power4.easeOut,
-})
+});
